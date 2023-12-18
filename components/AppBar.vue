bca36e6fa730ce9caf6c43c175e75e3f4aebc49d
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-18 17:24:35 -0500

    adds AppBar.vue

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,5 +1,9 @@
-<script setup></script>
-
 <template>
-  <v-app-bar :elevation="0" color="primary"></v-app-bar>
+  <v-app-bar :elevation="0" color="primary" title="Open AI Keyword Extractor">
+    <v-toolbar-items>
+      <v-btn icon class="mr-2">
+        <v-icon>mdi-lightbulb-on-outline</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
-<script setup></script>
-
 <template>
-  <div>hello</div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="8" offset-md="2">
+        <Hero />
+      </v-col>
+      <v-col cols="12" md="8" offset-md="2">
+        <Form />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
