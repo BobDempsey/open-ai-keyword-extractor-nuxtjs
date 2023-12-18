299e2cfad0b14963da208c17030c98f7fd60adce
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-18 17:25:06 -0500

    adds Form and Hero components

---
+++ components/Form.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-form ref="form">
+        <v-btn type="submit" color="primary" flat>submit</v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
---
+++ components/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-card
+    title="AI Keyword Extractor"
+    text="Paste in your text below and we'll automatically extract the keywords for you!"
+  >
+  </v-card>
+</template>
