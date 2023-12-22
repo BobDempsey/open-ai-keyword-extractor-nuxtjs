be2883ab07e809dae27bc26529009190af9b7000
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-22 10:55:52 -0500

    adds dark mode theme toggle in AppBar.vue

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,8 +1,25 @@
+<script setup>
+// vue => computed auto imported by nuxt
+
+// THEME
+import { useTheme } from "vuetify";
+const theme = useTheme();
+const isDark = computed(() => theme.global.current.value.dark);
+
+// THEME => toggle dark and light theme
+const toggleTheme = () => {
+  if (isDark.value) return (theme.global.name.value = "light");
+  return (theme.global.name.value = "dark");
+};
+</script>
+
 <template>
   <v-app-bar :elevation="0" color="primary" title="Open AI Keyword Extractor">
     <v-toolbar-items>
-      <v-btn icon class="mr-2">
-        <v-icon>mdi-lightbulb-on-outline</v-icon>
+      <!-- toggle theme -->
+      <v-btn icon class="px-8" @click.stop="toggleTheme">
+        <v-icon v-if="isDark">mdi-lightbulb-off-outline</v-icon>
+        <v-icon v-else>mdi-lightbulb-on-outline</v-icon>
       </v-btn>
     </v-toolbar-items>
   </v-app-bar>
