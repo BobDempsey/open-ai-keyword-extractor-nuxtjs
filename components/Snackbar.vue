f436c25dfedb82b5148611678cb997b248d7c1b0
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-22 16:26:03 -0500

    adds Snackbar.vue

---
+++ components/Snackbar.vue
@@ -0,0 +1,26 @@
+<script setup>
+// vue => computed auto imported by nuxt
+
+// pinia => useAppStore() && storeToRefs() auto imported by nuxt
+// pinia appStore
+const appStore = useAppStore();
+const { showSnackbar, snackbarColor, snackbarMessage } = storeToRefs(appStore);
+</script>
+
+<template>
+  <v-snackbar
+    location="top right"
+    :timeout="3000"
+    :color="snackbarColor || 'success'"
+    v-model="showSnackbar"
+    rounded="pill"
+  >
+    {{ snackbarMessage }}
+  </v-snackbar>
+</template>
+
+<style>
+.v-snackbar__content {
+  text-align: center !important;
+}
+</style>
