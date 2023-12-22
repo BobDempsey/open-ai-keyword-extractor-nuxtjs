4487eb0382e3faeb46ee0d99fbf012ebd9ca0ed1
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-22 10:55:02 -0500

    updates app.vue with dark mode styles

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup>
+// vue => computed auto imported by nuxt
+
+// theme
+import { useTheme } from "vuetify";
+const theme = useTheme();
+const isDark = computed(() => theme.global.current.value.dark);
+</script>
+
 <template>
-  <v-app class="bg-grey-lighten-4">
+  <v-app :class="{ 'bg-grey-lighten-5': !isDark }">
     <AppBar />
     <v-main>
       <NuxtPage />
