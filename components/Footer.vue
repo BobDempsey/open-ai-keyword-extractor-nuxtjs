315a2ab8b23e8a02d894a7808cee7710b739177d
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-22 16:25:33 -0500

    adds Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+// pinia => useAppStore() && storeToRefs() auto imported by nuxt
+
+// pinia appStore
+const appStore = useAppStore();
+const { portfolioUrl } = storeToRefs(appStore);
+</script>
+
+<template>
+  <v-footer border class="d-flex justify-center">
+    <!-- gitub repo btn -->
+    <v-btn
+      variant="text"
+      size="small"
+      color="primary"
+      :href="portfolioUrl"
+      target="_blank"
+      >🤓 {{ new Date().getFullYear() }} - Bob Dempsey</v-btn
+    >
+  </v-footer>
+</template>
