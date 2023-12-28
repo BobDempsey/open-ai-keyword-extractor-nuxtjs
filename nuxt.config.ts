d4e717cc3e03a5d811014ad41e26be6460b38257
Bob Dempsey <bobdempsey83@gmail.com>
2023-12-28 12:04:55 -0500

    begins adding custom vuetify theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~/assets/scss/style.scss"],
   app: {
     head: {
       charset: "utf-8",
@@ -21,7 +23,11 @@ export default defineNuxtConfig({
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
         // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }));
+        config.plugins.push(
+          vuetify({
+            autoImport: true,
+          })
+        );
       });
     },
     "@pinia/nuxt",
